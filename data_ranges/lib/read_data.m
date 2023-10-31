6ab7d23493a8a2384fea1686bde7b898fa85ec60
Seshu <scharla@purdue.edu>
2023-10-31 11:31:05 -0400

    Made it conditional

--- data_ranges/lib/read_data.m
+++ data_ranges/lib/read_data.m
@@ -1,22 +1,38 @@
-function data_struct = read_data()
-    data_struct = struct;
-    data_dir = "../../../Data";
-    %% drive data
+function data = read_data(name)
+    data_dir = "../../Data";
     drive_dir = data_dir + "/drive_data";
-    data_struct.drive.adt_15 = load(drive_dir + "/ADTransport_150814/ADTransport_150814_Day_File.mat");
-    data_struct.drive.adt_17 = load(drive_dir + "/ADTransport_170201/ADTransport_170201_dat_file.mat");
-    data_struct.drive.mes_15 = load(drive_dir + "/MesillaValley_150605/MesillaValley_150605_day_file.mat");
-    data_struct.drive.mes_18 = load(drive_dir + "/MesillaValley_180314/MesillaValley_180314_day_file.mat");
-    data_struct.drive.wer_15 = load(drive_dir + "/Werner_151111/Werner_151111_day_file.mat");
-    data_struct.drive.wer_20 = load(drive_dir + "/Werner_20171006/Werner_20171006_day_file.mat");
-    data_struct.drive.trw_15 = load(drive_dir + "/Transwest_150325/Transwest_150325_day_file.mat");
-    data_struct.drive.trw_16 = load(drive_dir + "/Transwest_161210/Transwest_161210_day_file.mat");
-    %% test cell data
     test_dir = data_dir + "/test_cell_data";
-    data_struct.test.aged_cftp = readtable(test_dir + "/g580040_Aged_cFTP.csv");
-    data_struct.test.aged_hftp = readtable(test_dir + "/g580041_Aged_hFTP.csv");
-    data_struct.test.aged_rmc  = readtable(test_dir + "/g580043_Aged_RMC.csv");
-    data_struct.test.dg_cftp   = readtable(test_dir + "/g577670_DG_cFTP.csv");
-    data_struct.test.dg_hftp   = readtable(test_dir + "/g577671_DG_hFTP.csv");
-    data_struct.test.dg_rmc    = readtable(test_dir + "/g577673_DG_RMC.csv");
+    %% drive data
+    if (name == "adt_15")
+        data = load(drive_dir + "/ADTransport_150814/ADTransport_150814_Day_File.mat");
+    elseif (name == "adt_17")
+        data = load(drive_dir + "/ADTransport_170201/ADTransport_170201_dat_file.mat");
+    elseif (name == "mes_15")
+        data = load(drive_dir + "/MesillaValley_150605/MesillaValley_150605_day_file.mat");
+    elseif (name == "mes_18")
+        data = load(drive_dir + "/MesillaValley_180314/MesillaValley_180314_day_file.mat");
+    elseif (name == "wer_15")
+        data = load(drive_dir + "/Werner_151111/Werner_151111_day_file.mat");
+    elseif (name == "wer_20")
+        data = load(drive_dir + "/Werner_20171006/Werner_20171006_day_file.mat");
+    elseif (name == "trw_15")
+        data = load(drive_dir + "/Transwest_150325/Transwest_150325_day_file.mat");
+    elseif (name == "trw_16")
+        data = load(drive_dir + "/Transwest_161210/Transwest_161210_day_file.mat");
+    %% test cell data
+    elseif (name == "aged_cftp")
+        data = readtable(test_dir + "/g580040_Aged_cFTP.csv");
+    elseif (name == "aged_hftp")
+        data = readtable(test_dir + "/g580041_Aged_hFTP.csv");
+    elseif (name == "aged_rmc")
+        data  = readtable(test_dir + "/g580043_Aged_RMC.csv");
+    elseif (name == "dg_cftp")
+        data = readtable(test_dir + "/g577670_DG_cFTP.csv");
+    elseif (name == "dg_hftp")
+        data = readtable(test_dir + "/g577671_DG_hFTP.csv");
+    elseif (name == "dg_rmc")
+        data = readtable(test_dir + "/g577673_DG_RMC.csv");
+    else
+        data = 0;
+    end
 end
